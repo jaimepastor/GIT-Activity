33782714341ab1679635bc0afb988d352e1194e6
JoshCrusader <joshua_cruzada@dlsu.edu.ph>
2018-09-24 14:55:50 +0800

    Made main a loop

--- main.py
+++ main.py
@@ -6,21 +6,23 @@ def make_rectangle(length, width):
 
 def make_triangle(size):
     pass
+
 def main():
-    print('Hello, please enter what to print')
-    print('1\tsquare')
-    print('2\trectangle')
-    print('3\ttriangle')
-    shape = (int)(input('Input: '))
-    if(shape == 1):
-        size = (int)(input('Input size:'))
-        make_sqaure(size)
-    elif(shape == 2):
-        length = (int)(input('Input length: '))
-        width = (int)(input('Input width: '))
-        make_rectangle(length, width)
-    elif(shape == 3):
-        size = (int)(input('Input size: '))
-        make_triangle(size)
+    while(True):
+        print('Hello, please enter what to print')
+        print('1\tsquare')
+        print('2\trectangle')
+        print('3\ttriangle')
+        shape = (int)(input('Input: '))
+        if(shape == 1):
+            size = (int)(input('Input size:'))
+            make_sqaure(size)
+        elif(shape == 2):
+            length = (int)(input('Input length: '))
+            width = (int)(input('Input width: '))
+            make_rectangle(length, width)
+        elif(shape == 3):
+            size = (int)(input('Input size: '))
+            make_triangle(size)
 
 main()
